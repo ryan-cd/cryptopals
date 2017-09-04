82c6e62a0d1fee6441b96ddaab8b267579487eef
s7aesis <ryancd774@gmail.com>
2017-09-04 12:16:04 -0400

    Finished hex to base64 conversion

--- set1/set1.go
+++ set1/set1.go
@@ -1,38 +1,57 @@
 package set1
 
 import (
-	"fmt"
+	"bytes"
 	"log"
 )
 
+//Hex2Base64 takes a hex string input and returns a base64 string
 func Hex2Base64(hexString string) (output string) {
 	b := byte('0')
-	bytes := []byte("")
+	byteSplice := []byte("")
+	var base64Buffer bytes.Buffer
+	paddingCount := 0
+
 	for i := 0; i < len(hexString); i += 2 {
-		b = 16 * HexDigitValue(hexString[i])
-		b += HexDigitValue(hexString[i+1])
-		bytes = append(bytes, b)
+		b = 16 * hexDigitValue(hexString[i])
+		b += hexDigitValue(hexString[i+1])
+		byteSplice = append(byteSplice, b)
 	}
 
-	// Pad the bytes to ensure divisible by 3
+	// Pad the byteSplice to ensure divisible by 3
 	for true {
-		if float32(len(bytes)/3) == float32(len(bytes))/3 {
+		if float32(len(byteSplice)/3) == float32(len(byteSplice))/3 {
 			break
 		} else {
-			bytes = append(bytes, 0)
+			byteSplice = append(byteSplice, 0)
+			paddingCount++
 		}
 	}
 
-	fmt.Print("Bytes are: ")
-	for _, element := range bytes {
-		fmt.Print(element, ", ")
-	}
-	fmt.Println()
+	var quadruple [4]byte // Store the 4 values that the 3 byteSplice convert to
+	for i := 0; i < len(byteSplice); i += 3 {
+		quadruple[0] = byteSplice[i] >> 2
+		quadruple[1] = ((byteSplice[i] & 3) << 4) | (byteSplice[i+1] >> 4)
+		if paddingCount >= 2 {
+			quadruple[2] = 64 // '=' character
+		} else {
+			quadruple[2] = ((byteSplice[i+1] & 15) << 2) | (byteSplice[i+2] >> 6)
+		}
+		if paddingCount >= 1 {
+			quadruple[3] = 64 // '=' character
+		} else {
+			quadruple[3] = byteSplice[i+2] & 63
+		}
 
-	return hexString
+		for _, item := range quadruple {
+			base64Buffer.WriteString(index2Base64(item))
+		}
+	}
+
+	return base64Buffer.String()
 }
 
-func HexDigitValue(hexDigit byte) (output byte) {
+func hexDigitValue(hexDigit byte) (output byte) {
 	if 48 <= hexDigit && hexDigit <= 57 {
 		return hexDigit - 48
 	} else if 97 <= hexDigit && hexDigit <= 102 {
@@ -41,4 +60,24 @@ func HexDigitValue(hexDigit byte) (output byte) {
 		log.Fatal("Unable to parse hex string")
 		return 0
 	}
+}
+
+func index2Base64(index byte) (base64String string) {
+	if index < 0 || index > 64 {
+		log.Fatal("Invalid index to convert to base 64")
+		return ""
+	}
+	dictionary := map[int]string{
+		0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F",
+		6: "G", 7: "H", 8: "I", 9: "J", 10: "K", 11: "L",
+		12: "M", 13: "N", 14: "O", 15: "P", 16: "Q", 17: "R",
+		18: "S", 19: "T", 20: "U", 21: "V", 22: "W", 23: "X",
+		24: "Y", 25: "Z", 26: "a", 27: "b", 28: "c", 29: "d",
+		30: "e", 31: "f", 32: "g", 33: "h", 34: "i", 35: "j",
+		36: "k", 37: "l", 38: "m", 39: "n", 40: "o", 41: "p",
+		42: "q", 43: "r", 44: "s", 45: "t", 46: "u", 47: "v",
+		48: "w", 49: "x", 50: "y", 51: "z", 52: "0", 53: "1",
+		54: "2", 55: "3", 56: "4", 57: "5", 58: "6", 59: "7",
+		60: "8", 61: "9", 62: "+", 63: "/", 64: "="}
+	return dictionary[int(index)]
 }
--- set1/set1_test.go
+++ set1/set1_test.go
@@ -9,13 +9,14 @@ func TestHex2Base64(t *testing.T) {
 		{"4d", "TQ=="},
 		{"4d61", "TWE="},
 		{"4d616e", "TWFu"},
+		{"ffffffffffff", "////////"},
 		{"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
 			"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"},
 	}
 	for _, testCase := range cases {
 		got := Hex2Base64(testCase.in)
 		if got != testCase.want {
-			t.Errorf("Hex2Base64(%q) == %q, want %q", testCase.in, got, testCase.want)
+			t.Errorf("Hex2Base64(%q) == \n%q, want \n%q", testCase.in, got, testCase.want)
 		}
 	}
 }
